c84512c9d644c4247153ed1f34a32e0af4e418a4
Aayush Sharma <sharmagiriaayush@gmail.com>
2018-07-09 14:19:44 +0545

    Reviews

--- reviews.py
+++ reviews.py
@@ -7,14 +7,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#adding incognito
+# adding incognito
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 
-#opening an instance of browser
-driver = webdriver.Chrome(executable_path='/Users/aayushsharma/python/voterlist/chromedriver',chrome_options=option)
+# opening an instance of browser
+driver = webdriver.Chrome(executable_path='/Users/aayushsharma/python/voterlist/chromedriver', chrome_options=option)
 driver.get("http://www.educatenepal.com/college_review/all_reviews")
-review={}
+review = {}
 invoker = driver.execute_script('update_search(11,0);')
 # element = WebDriverWait(driver,40).until(EC.presence_of_all_elements_located((By.CLASS_NAME,"search_result_title")))
 # page_html = driver.page_source
